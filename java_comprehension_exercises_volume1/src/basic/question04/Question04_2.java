96590a18cc5cb2beb6757806fb4a7ce80de9e19f
kureaa <kureaa@C010422904.zipangu.cac.co.jp>
2026-04-16 11:51:36 +0900

    Question04完了

--- java_comprehension_exercises_volume1/src/basic/question04/Question04_2.java
+++ java_comprehension_exercises_volume1/src/basic/question04/Question04_2.java
@@ -11,7 +11,15 @@ public class Question04_2 {
 	public static void main(String[] args) throws IOException {
 
 		// TODO:ここに実装
-		
+		int[] ageGroup = { 23, 33, 26, 21, 25, 22 };
+
+		System.out.println("4番目の人の年齢を入力してください。");
+
+		int temporaryAge = 39;
+		ageGroup[3] = temporaryAge;
+
+		System.out.println("4番目の人の年齢は" + ageGroup[3] + "歳です。");
+		System.out.println("人数は" + ageGroup.length + "人です。");
 	}
 
 }
